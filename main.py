ae937d570c72a5c560fc5f011a6433cb79c16dd1
matthewrrayl <104227914+matthewrrayl@users.noreply.github.com>
2022-05-11 18:08:22 -0500

    looped image analyzer/view image option

--- main.py
+++ main.py
@@ -4,14 +4,30 @@
 import cv2
 from deepface import DeepFace
 
-inputimage = input('Enter image path: ')
-image = cv2.imread(inputimage)
+
+def main():
+    inputimage = input('Enter image path: ')  # Input path to image
+    image = cv2.imread(inputimage)
+    check = 1
+    while check == 1:
+        analyze = DeepFace.analyze(image, actions=('emotion', 'age', 'gender', 'race'))
+        print(f'Dominant Emotion: {analyze["dominant_emotion"]}')
+        print(f'Age: {analyze["age"]}')
+        print(f'Gender: {analyze["gender"]}')
+        print(f'Race: {analyze["dominant_race"]}')
+
+        view = input('View Image? y/n: ')  # Input y or n to see the inputted image
+        view = view.strip().lower()
+        if view == 'y':
+            cv2.imshow(inputimage, image)
+            cv2.waitKey(0)  # Hit any key on keyboard to close the image
+        repeat = input('Repeat? y/n: ')  # Input y or n to analyze new image
+        repeat = repeat.strip().lower()
+        if repeat == 'y':
+            inputimage = input('Enter image path: ')
+        elif repeat == 'n':
+            check += 1
 
 
-analyze = DeepFace.analyze(image, actions=('emotion', 'age', 'gender', 'race'))
-print(f'Dominant Emotion: {analyze["dominant_emotion"]}')
-print(f'Age: {analyze["age"]}')
-print(f'Gender: {analyze["gender"]}')
-print(f'Race: {analyze["dominant_race"]}')
-cv2.imshow(inputimage, image)
-cv2.waitKey(0) #Press any key on your keyboard to close image
+if __name__ == '__main__':
+    main()
